f96fa6c660cba1b8539b5b090046d1e0885934e6
abhishek2507 <abhibuddy1234@gmail.com>
2020-08-21 21:00:07 +0530

    Maximum length of a substring

---
+++ Day8/17.Longest_common_substring.py
@@ -0,0 +1,17 @@
+'''
+  I am just practicing more and more questions but putting uyp the codes which are really of that important
+'''
+
+def LCSubStr(X, Y, m, n):  
+    LCSuff = [[0 for k in range(n+1)] for l in range(m+1)]        
+    result = 0 
+    for i in range(m + 1): 
+        for j in range(n + 1): 
+            if (i == 0 or j == 0): 
+                LCSuff[i][j] = 0
+            elif (X[i-1] == Y[j-1]): 
+                LCSuff[i][j] = LCSuff[i-1][j-1] + 1
+                result = max(result, LCSuff[i][j]) 
+            else: 
+                LCSuff[i][j] = 0
+    return result 
